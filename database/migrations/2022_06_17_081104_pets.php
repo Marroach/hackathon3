f9f2d322e022d8b2c7ab8af9fdce7a9405260592
Marian <marsaldana@Marians-MacBook-Air.local>
2022-06-17 10:24:19 +0200

    table pets

---
+++ database/migrations/2022_06_17_081104_pets.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pets', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('image_id')->nullable();
+            $table->bigInteger('owner_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('species')->nullable();
+            $table->string('breed')->nullable();
+            $table->integer('age')->nullable();
+            $table->integer('weight')->nullable();
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pets', function (Blueprint $table) {
+            //
+        });
+    }
+};
